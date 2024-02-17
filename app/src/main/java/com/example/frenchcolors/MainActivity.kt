2ee4f962ebe89a21fc03c80b1b3b1e3953f3b3e4
Yukio <elsie.atieno@student.moringaschool.com>
2024-02-17 13:18:01 +0300

    Added functionality

--- app/src/main/java/com/example/frenchcolors/MainActivity.kt
+++ app/src/main/java/com/example/frenchcolors/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.frenchcolors
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+     fun SayTheColor(view: View){
+        val clickedBtn : Button = view as Button
+
+         var mediaPlayer : MediaPlayer = MediaPlayer.create(
+             this,
+             resources.getIdentifier(clickedBtn.tag.toString(), "raw", packageName)
+         )
+         mediaPlayer.start()
+    }
+
 }
